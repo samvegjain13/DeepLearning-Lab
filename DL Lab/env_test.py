4abb420715dc371c9686a644070a838977611b7e
samvegjain13 <samvegjain014@gmail.com>
2026-02-02 10:49:47 +0530

    Add files via upload

---
+++ DL Lab/env_test.py
@@ -0,0 +1,7 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import sklearn
+
+print("NumPy:", np.__version__)
+print("Matplotlib:", plt.matplotlib.__version__)
+print("Scikit-learn:", sklearn.__version__)
